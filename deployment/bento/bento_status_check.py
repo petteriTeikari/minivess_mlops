ba79ac9aa83e20983d07d234f34aa0a14f291180
Petteri Teikari <petteri.teikari@gmail.com>
2023-11-08 22:17:50 +0000

    bentoml build status check

--- deployment/bento/bento_status_check.py
+++ deployment/bento/bento_status_check.py
@@ -3,20 +3,20 @@ import os
 import sys
 from loguru import logger
 
-from src.log_ML.bentoml_log.bentoml_log_models import import_bento_from_s3, get_mlflow_run_id_of_bento, \
-    import_bento_model_from_s3
-from src.log_ML.mlflow_log.mlflow_admin import import_best_model_from_model_registry
-from src.log_ML.mlflow_log.mlflow_init import authenticate_mlflow, init_mlflow
-from src.utils.config_utils import get_service_uris, get_repo_dir
-from src.utils.dict_utils import cfg_key
-
 bentoml_path = os.path.dirname(os.path.abspath(__file__))
 deployment_path = os.path.split(bentoml_path)[0]
 project_path = os.path.split(deployment_path)[0]
 logger.debug('project_path = {}'.format(project_path))
 sys.path.insert(0, project_path)  # so that src. is imported correctly also in VSCode by default
 
-
+from src.log_ML.bentoml_log.bentoml_containarize import containarize_bento
+from src.log_ML.bentoml_log.bentoml_log_models import import_bento_from_s3, get_mlflow_run_id_of_bento, \
+    import_bento_model_from_s3
+from src.log_ML.bentoml_log.bentoml_utils import get_latest_bento_docker
+from src.log_ML.mlflow_log.mlflow_admin import import_best_model_from_model_registry
+from src.log_ML.mlflow_log.mlflow_init import authenticate_mlflow, init_mlflow
+from src.utils.config_utils import get_service_uris, get_repo_dir
+from src.utils.dict_utils import cfg_key
 from src.utils.general_utils import print_dict_to_logger
 
 def parse_args_to_dict():
@@ -26,6 +26,9 @@ def parse_args_to_dict():
                         help="The name that you used to log models to MLflow Model Registry")
     parser.add_argument('-bentoml_log', '--bentoml_model_name', type=str, required=False,
                         default='minivess-segmentor',
+                        help="The name that you used to log model to BentoML Model Store")
+    parser.add_argument('-o', '--output', type=str, required=False,
+                        default='is_docker_old', choices=['is_docker_old'],
                         help="The name that you used to log model to BentoML Model Store")
     args_dict = vars(parser.parse_args())
     logger.info('Parsed input arguments:')
@@ -79,5 +82,21 @@ if __name__ == '__main__':
     # Compare Registered MLFlow model and the Bento imported from S3
     from_same_run = compare_mlflow_and_bento(mlflow_model, bento_model)
 
+    # Get the latest Bento (from S3), ADD THIS IF NEEDED/WANTED
+
     # Get the latest Bento Docker and see its run_id
-    get_latest_bento()
+    docker_built_from_best_run_id = (
+        get_latest_bento_docker(docker_image=cfg_key(service_uris, 'BENTOML', 'docker_image'),
+                                run_id_tag=mlflow_model['latest_version'].run_id))
+
+    # Build Bento with Containarize
+    docker_built_from_best_run_id = False
+    if not docker_built_from_best_run_id:
+        containarize_bento(bento_model=bento_model,
+                           docker_image=cfg_key(service_uris, 'BENTOML', 'docker_image'))
+
+    if args['output'] == 'is_docker_old':
+        if docker_built_from_best_run_id:
+            sys.exit(0)  ## echo $? from bash
+        else:
+            sys.exit(1)  ## echo $? from bash
--- src/log_ML/bentoml_log/bentoml_utils.py
+++ src/log_ML/bentoml_log/bentoml_utils.py
@@ -1,3 +1,6 @@
+import json
+import subprocess
+
 from loguru import logger
 
 
@@ -9,3 +12,50 @@ def parse_tag_from_exception(e):
     except Exception as e:
         logger.debug('Failed to parse tag from BentoML exception, e = {}')
     return tag
+
+
+def get_latest_bento_docker(docker_image: str,
+                            run_id_tag: str,
+                            host: str = 'https://registry.hub.docker.com/v2/repositories'):
+
+    def get_non_latest_tag(json_object: dict):
+        images_out, tags = [], []
+        for image in json_object['results']:
+            if image['name'] != 'latest':
+                logger.debug('Found tag (run_id) = {}'.format(image['name']))
+                images_out.append(image)
+                tags.append(image['name'])
+        logger.info('Found a total of {} non-"latest" tags'.format(len(images_out)))
+        return images_out, tags
+
+    # import docker
+    # try:
+    #     # Installation error occurred, docker seemes to be installed via "poetry add",
+    #     # but still cannot use it?
+    #     client = docker.from_env()
+    # except Exception as e:
+    #     logger.debug(' docker.__path__ = {}'.format(docker.__path__))
+    #     logger.error('Failed to initialize Docker (Python) client, e = {}'.format(e))
+    #     # OSError: Failed to initialize Docker (Python) client, e = module 'docker' has no attribute 'from_env'
+    #     raise OSError('Failed to initialize Docker (Python) client, e = {}'.format(e))
+
+    # Use the Docker CLI API with jq (sudo apt install jq)
+    # https://forums.docker.com/t/how-can-i-list-tags-for-a-repository/32577/10
+    # curl 'https://registry.hub.docker.com/v2/repositories/petteriteikari/minivess-segmentor/tags/'|jq '."results"[]["name"]'
+
+    hub_path = f'{host}/{docker_image}/tags/'
+    command = f'curl "{hub_path}"|jq'
+    data = subprocess.run(command, capture_output=True, shell=True, text=True)
+    json_object = json.loads(data.stdout)
+
+    images_out, tags = get_non_latest_tag(json_object)
+
+    if run_id_tag in tags:
+        docker_built_from_best_run_id = True
+    else:
+        last_push = images_out[0]['tag_last_pushed']
+        logger.info('Bento image on Docker Hub seems to be behind the best model on MLFlow, '
+                    'Last push = {}'.format(last_push))
+        docker_built_from_best_run_id = False
+
+    return docker_built_from_best_run_id
